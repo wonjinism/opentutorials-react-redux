bfa78968cb005dc8a65c620eafd8ebbff46ec127
Wonjin Kim <wonjinism@gmail.com>
2021-01-09 23:15:38 +0900

    redux

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import {createStore} from 'redux'
+const initState = {
+    mode: 'WELCOME',
+    welcome_content: {
+        title: 'WEB',
+        desc: 'Hello, WEB'
+    },
+    selected_content_id: 1,
+    contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is ...'},
+        {id: 2, title: 'CSS', desc: 'CSS is ...'},
+        {id: 3, title: 'JS', desc: 'JS is ...'},
+    ]
+}
+
+function reducer(state=initState, action) {
+    return state
+}
+
+export default createStore(reducer)
